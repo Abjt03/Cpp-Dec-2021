ab7243ea4591dae511158f05a9d5b4333efd8696
Abjt03 <95538488+Abjt03@users.noreply.github.com>
2021-12-30 18:59:30 +0530

    Create time-disp-class.cpp

---
+++ time-disp-class.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+class Time {
+    int h,m,s;
+    public:
+    void read();
+    void print();
+}T;
+void Time::read() {
+    cout<<"Hours ?"; cin>>h;
+    cout<<"Minutes ?"; cin>>m;
+    cout<<"Seconds ?"; cin>>s;
+}
+void Time::print() {
+    cout<<setw(2)<<setfill('0')<<h<<':';
+    cout<<setw(2)<<setfill('0')<<m<<':';
+    cout<<setw(2)<<setfill('0')<<s;
+}
+int main() {
+	T.read();
+	T.print();
+	return 0;
+}
